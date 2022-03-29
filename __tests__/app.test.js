ae68d7c783ddd3fbb83486e2b6b06726bd45f16d
ajoy267 <andrew.joy12@hotmail.com>
2022-03-29 11:50:23 -0700

    wrote my route, setupSQL, and create user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,12 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+// Dummy User
+const mockUser = {
+  email: 'test@example.com',
+  password: 'abcdefg',
+};
+
 describe('backend-top-secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +16,12 @@ describe('backend-top-secrets routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should create a new user with an email/password', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({ id: expect.any(String), email });
   });
 });
