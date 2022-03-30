3d3dee1fc6168d2ce8c57a8165fea54d0cc2659a
ajoy267 <andrew.joy12@hotmail.com>
2022-03-30 14:09:27 -0700

    wrote my test for creating a secret

---
+++ __tests__/secrets.test.js
@@ -0,0 +1,39 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+// Dummy User
+const mockUser = {
+  email: 'test@example.com',
+  password: 'abcdefg',
+};
+
+describe('backend-top-secrets routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should create a secret', async () => {
+    const agent = request.agent(app);
+    await UserService.create({ ...mockUser });
+    await agent.post('/api/v1/users/sessions').send(mockUser);
+
+    const expected = {
+      title: 'Assingment',
+      description: 'this is hard',
+    };
+
+    const res = await agent.post('/api/v1/secrets').send(expected);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...expected,
+      createdAt: expect.any(String),
+    });
+  });
+});
